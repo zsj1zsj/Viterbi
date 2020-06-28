2488bf6451e773cdfba25d309753b7ff58de2c98
zsj1zsj <zsj1zsj@gmail.com>
2020-06-28 23:42:02 +0800

    add comments
    add comments for understanding

--- src/com/hankcs/algorithm/Viterbi.java
+++ src/com/hankcs/algorithm/Viterbi.java
@@ -20,6 +20,7 @@ public class Viterbi
         double[][] V = new double[obs.length][states.length];
         int[][] path = new int[states.length][obs.length];
 
+        #第一天所有状态初始化，start_p * emit_p
         for (int y : states)
         {
             V[0][y] = start_p[y] * emit_p[y][obs[0]];
@@ -30,12 +31,13 @@ public class Viterbi
         {
             int[][] newpath = new int[states.length][obs.length];
 
-            for (int y : states)
+            for (int y : states) 
             {
                 double prob = -1;
                 int state;
                 for (int y0 : states)
                 {
+                    // 计算S_{t-1}到S_t的概率, y0是S{t-1}的状态， y是S_{t}
                     double nprob = V[t - 1][y0] * trans_p[y0][y] * emit_p[y][obs[t]];
                     if (nprob > prob)
                     {
@@ -43,7 +45,8 @@ public class Viterbi
                         state = y0;
                         // 记录最大概率
                         V[t][y] = prob;
-                        // 记录路径
+                        // 记录最佳路径,当前状态的的路径来由
+                        //path[state]是t-1的
                         System.arraycopy(path[state], 0, newpath[y], 0, t);
                         newpath[y][t] = y;
                     }
@@ -55,6 +58,7 @@ public class Viterbi
 
         double prob = -1;
         int state = 0;
+        //遍历所有状态， 找出最佳路径, path只保留当前路径。
         for (int y : states)
         {
             if (V[obs.length - 1][y] > prob)
